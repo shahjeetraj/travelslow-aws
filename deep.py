c2a9a80b7dfa814d3b681fb64995e6a238dee453
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-08-21 18:02:54 +0530

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -1,5 +1,5 @@
 deep = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-if deep == "42" or deep == "forty two" or deep == "forty-two":
+if deep.lower() == "42" or deep == "forty two" or deep == "forty-two":
     print("Yes")
 else:
     print("No")
