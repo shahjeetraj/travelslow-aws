1526db6f8b2dfd2393ec44a8dec0339f97b52bf4
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-08-24 18:22:44 +0530

    automated commit by check50 [check50=True]

--- fuel.py
+++ fuel.py
@@ -2,19 +2,20 @@ import re
 
 def main():
     fuel = ''
-    while not re.match("^[0-4]/[124]$",fuel):
+    while not re.match("^([0-9]+)/{1}([1-9]+)$",fuel):
         fuel = input("Fraction: ").strip()
     else:
         print(get_fuelpc(fuel))
 
 def get_fuelpc(f):
+    f = f.split('/')
     x = int(f[0])
-    y = int(f[2])
-    if x == y == 4:
+    y = int(f[1])
+    if x == y:
         return "F"
     elif x == 0:
         return "E"
     else:
-        return str(int(x/y*100))+"%"
+        return str(round(x/y*100))+"%"
 
 main()
