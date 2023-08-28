0d78c6f99153fc8d52d98853317671c03862e9af
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-08-28 19:22:17 +0530

    automated commit by check50 [check50=True]

--- professor.py
+++ professor.py
@@ -1,57 +1,52 @@
 import random
-level=0
-wrong_a = [0,0,0,0,0,0,0,0,0,0]
+
 def main():
     level = get_level()
-    #print (level)
     score = 0
-    if level in [1,2,3]:
-        for q in range(10):
-            qa_list = generate_q(level)
-            #print(qa_list)
-            while wrong_a[q] < 3:
-                ans = input(f"{qa_list[0]} = ")
-                try:
-                    if int(ans) != qa_list[1]:
-                        wrong_a[q] += 1
-                        print("EEE")
-                    else:
-                        score += 1
-                        break
-                except ValueError:
-                    wrong_a[q] += 1
+
+    for _ in range(10):
+        x = generate_integer(level)
+        y = generate_integer(level)
+        result = x + y
+        attempt = 0
+
+        while attempt < 3:
+            try:
+                answer = int(input(f"{x} + {y} = "))
+                if answer == result:
+                    score += 1
+                    break
+                else:
                     print("EEE")
-            else:
-                print(f"{qa_list[0]} = {qa_list[1]}")
-        return(score)
+                    attempt += 1
+            except ValueError:
+                print("EEE")
+                attempt += 1
+
+        if attempt == 3:
+            print(f"{x} + {y} = {result}")
+
+    print(score)
 
 
 def get_level():
-    try:
-        while True:
+    while True:
+        try:
             level = int(input("Level: "))
             if level in [1,2,3]:
                 return level
-    except ValueError:
-        get_level()
+        except ValueError:
+            continue
 
-def generate_q(l):
-    if l == 1:
-        question = "".join(str(random.randint(0,9))+" + "+str(random.randint(0,9)))
-        broken = question.split(" + ")
-        answer = int(broken[0]) + int(broken[1])
-        return [question,answer]
-    elif l ==2:
-        question = "".join(str(random.randint(10,99))+" + "+str(random.randint(10,99)))
-        broken = question.split(" + ")
-        answer = int(broken[0]) + int(broken[1])
-        return [question,answer]
+
+def generate_integer(level):
+    if level == 1:
+        return random.randint(0,9)
+    elif level == 2:
+        return random.randint(10,99)
     else:
-        question = "".join(str(random.randint(100,999))+" + "+str(random.randint(100,999)))
-        broken = question.split(" + ")
-        answer = int(broken[0]) + int(broken[1])
-        return [question,answer]
+        return random.randint(100,999)
+
 
 if __name__ == "__main__":
-    s=main()
-    print(f"Score: {str(s)}")
+    main()
