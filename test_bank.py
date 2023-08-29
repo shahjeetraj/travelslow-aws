457b8e63a5782fad7dc7b3d789fcf9eb72f5c2fd
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-08-29 17:51:50 +0530

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -7,12 +7,15 @@ def main():
 
 def test_for_100():
     assert value("Whats up?") == "$100"
+    assert value("What's up") == "$100"
 
 def test_for_20():
     assert value("Hey, there") == "$20"
+    assert value("hey") == "$20"
 
 def test_for_0():
     assert value("Hello, Kramer!") == "$0"
+    assert value("Hello") == "$0"
 
 if __name__ == "__main__":
     main()
