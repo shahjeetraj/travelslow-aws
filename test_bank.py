73c7a1a1a9d0497f0d7359f7ffdcc93f371cc152
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-08-29 18:02:01 +0530

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -1,4 +1,5 @@
 from bank import value
+import pytest
 
 def test_for_100():
     assert value("Whats up?") == "$100"
@@ -14,6 +15,30 @@ def test_for_0():
     assert value("Hello") == "$0"
     assert value("HELLO") == "$0"
 
+def test_int():
+    assert value('1') == '$100'
+
+def test_0():
+    assert value('Hello, how"s going?') == '$0'
+    assert value('HELLO') == '$0'
+
+def test_20():
+    assert value('h') == '$20'
+    assert value('hey, good') == '$20'
+    assert value('HOLA ') == '$20'
+
+def test_100():
+    assert value('wow, hello') == '$100'
+    assert value('LalhalHa') == '$100'
+
+def test_punct():
+    assert value(' .,;: "') == '$100'
+
 test_for_100()
 test_for_20()
 test_for_0()
+test_int()
+test_punct()
+test_100()
+test_20()
+test_0()
