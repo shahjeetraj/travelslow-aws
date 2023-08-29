4e4a1a5afe2fdd2b3bbb51971958ca4b69f14a1d
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-08-29 18:12:05 +0530

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -1,37 +1,37 @@
 from bank import value
 
 def test_bank_for_100():
-    assert value("Whats up?") == "$100"
-    assert value("What's up") == "$100"
+    assert value("Whats up?") == 100
+    assert value("What's up") == 100
 
 def test_for_20():
-    assert value("Hey, there") == "$20"
-    assert value("hey") == "$20"
-    assert value("HEY") == "$20"
+    assert value("Hey, there") == 20
+    assert value("hey") == 20
+    assert value("HEY") == 20
 
 def test_for_0():
-    assert value("Hello, Kramer!") == "$0"
-    assert value("Hello") == "$0"
-    assert value("HELLO") == "$0"
+    assert value("Hello, Kramer!") == 0
+    assert value("Hello") == 0
+    assert value("HELLO") == 0
 
 def test_int():
-    assert value('1') == '$100'
+    assert value('1') == 100
 
 def test_0():
-    assert value('Hello, how"s going?') == '$0'
-    assert value('HELLO') == '$0'
+    assert value('Hello, how"s going?') == 0
+    assert value('HELLO') == 0
 
 def test_20():
-    assert value('h') == '$20'
-    assert value('hey, good') == '$20'
-    assert value('HOLA ') == '$20'
+    assert value('h') == 20
+    assert value('hey, good') == 20
+    assert value('HOLA ') == 20
 
 def test_100():
-    assert value('wow, hello') == '$100'
-    assert value('LalhalHa') == '$100'
+    assert value('wow, hello') == 100
+    assert value('LalhalHa') == 100
 
 def test_punct():
-    assert value(' .,;: "') == '$100'
+    assert value(' .,;: "') == 100
 
 test_bank_for_100()
 test_for_20()
