51634e23d36df90e3884f8481ad4f3452b2cc849
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-08-29 18:14:27 +0530

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -14,30 +14,30 @@ def test_for_0():
     assert value("Hello") == 0
     assert value("HELLO") == 0
 
-def test_int():
-    assert value('1') == 100
+#def test_int():
+#    assert value('1') == 100
 
-def test_0():
-    assert value('Hello, how"s going?') == 0
-    assert value('HELLO') == 0
+#def test_0():
+#    assert value('Hello, how"s going?') == 0
+#    assert value('HELLO') == 0
 
-def test_20():
-    assert value('h') == 20
-    assert value('hey, good') == 20
-    assert value('HOLA ') == 20
+#def test_20():
+#    assert value('h') == 20
+#    assert value('hey, good') == 20
+#    assert value('HOLA ') == 20
 
-def test_100():
-    assert value('wow, hello') == 100
-    assert value('LalhalHa') == 100
+#def test_100():
+#    assert value('wow, hello') == 100
+#    assert value('LalhalHa') == 100
 
-def test_punct():
-    assert value(' .,;: "') == 100
+#def test_punct():
+#    assert value(' .,;: "') == 100
 
 test_bank_for_100()
 test_for_20()
 test_for_0()
-test_int()
-test_punct()
-test_100()
-test_20()
-test_0()
+#test_int()
+#test_punct()
+#test_100()
+#test_20()
+#test_0()
