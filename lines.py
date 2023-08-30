ecb4d7b5f8245623951cbee96ce1d2cbe2e720f3
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-08-30 15:50:34 +0530

    automated commit by check50 [check50=True]

--- lines.py
+++ lines.py
@@ -4,16 +4,16 @@ import os
 def main():
     if len(sys.argv) == 1:
         print("Too few command-line arguments")
-        sys.exit()
+        sys.exit(1)
     elif len(sys.argv) > 2:
         print("Too many command-line arguments")
-        sys.exit()
+        sys.exit(1)
     elif not os.path.exists(sys.argv[1]):
         print("File does not exist")
-        sys.exit()
+        sys.exit(1)
     elif not sys.argv[1].endswith(".py"):
         print("Not a Python file")
-        sys.exit()
+        sys.exit(1)
     else:
         print(cnt_lines(sys.argv[1]))
 
