59868bcc49cd2c29d18bf46f5d5390b58327418d
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-09-04 19:45:32 +0530

    automated commit by check50 [check50=True]

--- test_um.py
+++ test_um.py
@@ -9,6 +9,7 @@ def test_valid():
     assert count("um?") == 1
     assert count("Um, thanks for the album.") == 1
     assert count("Um, thanks, um...") == 2
+    assert count("um") == 1
 
 def test_invalid():
     assert count("yum") == 0
--- um.py
+++ um.py
@@ -7,7 +7,7 @@ def main():
 
 
 def count(s):
-    return len(re.findall(r'\b\W*um\W',s.lower()))
+    return len(re.findall(r"\b\W*um\W*",s,re.IGNORECASE))
 
 
 if __name__ == "__main__":
