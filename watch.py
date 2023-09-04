5a04b3da5e0cdcca425ce2c5c7e4fa081b303874
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-09-04 13:24:01 +0530

    automated commit by check50 [check50=True]

---
+++ watch.py
@@ -0,0 +1,24 @@
+import re
+import sys
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    if re.search(r'(https?://)?(www\.)?'
+                '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+                '(watch\?.*?(?=v=)v=|embed/|v/|.+\?v=)?([^&=%\?]{11})',s) != None:
+        x = re.search(r'(https?://)?(www\.)?'
+                '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+                '(watch\?.*?(?=v=)v=|embed/|v/|.+\?v=)?([^&=%\?]{11})',s)
+        #print(x.group())
+        found_list = re.split("\/",x.group())
+        return "".join("https://youtu.be/"+found_list[-1])
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    main()
