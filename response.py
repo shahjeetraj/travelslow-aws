929253eb7cab616f2c01d1b352b1209a92f4df23
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-09-04 19:59:24 +0530

    automated commit by check50 [check50=True]

---
+++ response.py
@@ -0,0 +1,10 @@
+import validators
+
+def main():
+    if validators.email(input("What's your email address? ")):
+        print("Valid")
+    else:
+        print("Invalid")
+
+if __name__ == "__main__":
+    main()
