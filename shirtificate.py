1b15cdacd5f56dc9dc60b829e54810a395a775c5
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-09-05 19:45:29 +0530

    automated commit by check50 [check50=True]

--- shirtificate.py
+++ shirtificate.py
@@ -5,7 +5,16 @@ class Shirtificate(FPDF):
 		super().__init__()
 		self.add_page()
 		self.set_font("helvetica", "B", 50)
-		self.cell(0, 60, "CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", align="C")
+		self.cell(
+            190,
+            9,
+            "CS50 Shirtificate",
+            border=1,
+            new_x="LMARGIN",
+            new_y="NEXT",
+            align="C",
+            fill=False
+        )
 		self.image("shirtificate.png", w=self.epw)
 		self.set_font_size(30)
 		self.set_text_color(255, 255, 255)
