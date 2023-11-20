68149df8a847e92bb9019d3c30af00f27e4ca2c1
shahjeetraj <shahjeetraj@users.noreply.github.com>
2023-11-20 19:16:02 +0530

    automated commit by check50 [check50=True]

--- password.c
+++ password.c
@@ -6,6 +6,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int len(string word);
 bool valid(string password);
 
 int main(void)
@@ -29,23 +30,32 @@ bool valid(string password)
     int lower = 0;
     int number = 0;
     int symbol = 0;
-    for (int i = 0; i < 100; i++)
+    int plen = len(password);
+    for (int i = 0; i < plen; i++)
     {
         if (isupper(password[i]))
         {
             upper++;
+            //printf("%c\n",password[i]);
+            //printf("%i\n",isupper(password[i]));
         }
         else if (islower(password[i]))
         {
             lower++;
+            //printf("%c\n",password[i]);
+            //printf("%i\n",islower(password[i]));
         }
         else if (isdigit(password[i]))
         {
             number++;
+            //printf("%c\n",password[i]);
+            //printf("%i\n",isdigit(password[i]));
         }
         else if (ispunct(password[i]))
         {
             symbol++;
+            //printf("%c\n",password[i]);
+            //printf("%i\n",isdigit(password[i]));
         }
         else if (upper >= 1 && lower >= 1 && number >= 1 && symbol >= 1)
         {
@@ -53,6 +63,7 @@ bool valid(string password)
         }
         else if (isspace(password[i]))
         {
+            //printf("%c\n",password[i]);
             break;
         }
         else
@@ -62,11 +73,23 @@ bool valid(string password)
     }
     if (upper >= 1 && lower >= 1 && number >= 1 && symbol >= 1)
     {
+        //printf("lower %i upper %i number%i symbol%i\n", lower, upper, number, symbol);
         return true;
     }
     else
     {
-        printf("lower %i upper %i number%i symbol%i\n", lower, upper, number, symbol);
+        //printf("lower %i upper %i number%i symbol%i\n", lower, upper, number, symbol);
         return false;
     }
+}
+
+int len(string word)
+{
+    int a = 0;
+    do
+    {
+        a++;
+    }
+    while(word[a]!=0);
+    return a;
 }
