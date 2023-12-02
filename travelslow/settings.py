92ac28030b8093418e67e39e83505019b4111661
Jeetraj Shah <142012983+shahjeetraj@users.noreply.github.com>
2023-12-02 12:15:49 +0530

    CORS Handling
    Changes in settings.py and requirements.txt

--- travelslow/settings.py
+++ travelslow/settings.py
@@ -49,6 +49,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'destinations',
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -61,12 +62,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS: True
 
 ROOT_URLCONF = 'travelslow.urls'
 
