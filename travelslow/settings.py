4e54c683300492df7dd35843fa943d96e441df53
Jeetraj Shah <142012983+shahjeetraj@users.noreply.github.com>
2023-12-10 15:41:43 +0530

    Update settings.py
    db changes

--- travelslow/settings.py
+++ travelslow/settings.py
@@ -96,10 +96,21 @@ WSGI_APPLICATION = 'travelslow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tsdb',
+        'USER': 'shahjeetraj',
+        'PASSWORD': '2008',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -152,6 +163,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
